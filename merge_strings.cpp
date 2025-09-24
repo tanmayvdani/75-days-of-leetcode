97dbdcb05fbd70ae190d8762d6a6b55142c94581
Wierdcreations <wierdtangaming@gmail.com>
2025-09-24 22:52:24 +0530

    Day 1

---
+++ merge_strings.cpp
@@ -0,0 +1,18 @@
+using namespace std;
+
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        string merged;
+        int maxL = max(word1.length(),word2.length());
+        for (int i=0;i< maxL; i++) {
+                if (i < word1.length()) {
+                    merged += word1[i];
+                }
+                if (i < word2.length()) {
+                    merged += word2[i];
+                }
+            }
+            return merged;
+        }
+};
